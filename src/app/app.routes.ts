0688e5d00eddfc2d593157c549188a6d993c4830
ROBERTO TIRON <robtiron@emeal.nttdata.com>
2025-06-18 09:06:04 +0200

    init basic config pipesapp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'basic',
+    title: 'Basic Pipes',
+    loadComponent: () => import('./pipes/pages/basic-page/basic-page')
+  },
+  {
+    path: 'numbers',
+    title: 'Numbers Pipes',
+    loadComponent: () => import('./pipes/pages/numbers-page/numbers-page')
+  },
+  {
+    path: 'uncommon',
+    title: 'Uncommon Pipes',
+    loadComponent: () => import('./pipes/pages/uncommon-page/uncommon-page')
+  },
+  {
+    path: 'custom',
+    title: 'Custom Pipes',
+    loadComponent: () => import('./pipes/pages/custom-page/custom-page')
+  },
+
+
+];
